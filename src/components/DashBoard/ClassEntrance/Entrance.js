64800654002df153748f4fe1b33b0379b1145146
shirinbehnami <hhagh2002@gmail.com>
2022-08-07 22:15:40 +0430

    basic components added.

---
+++ src/components/DashBoard/ClassEntrance/Entrance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Entrance = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Entrance;
---
+++ src/components/DashBoard/UserInfo/UserInfo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserInfo = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default UserInfo;
